74fb357eca8c979614f5226df494c39ba46ee37c
ivoivkovich <ivoivkovich@gmail.com>
2026-04-21 19:45:25 +0300

    HW V2

--- src/Book.java
+++ src/Book.java
@@ -1,18 +1,20 @@
 public class Book {
     private String name;
     private int year;
+    String author;
 
 
     public Book (){
         this.name = name;
         this.year = year;
-
+        this.author = author;
     }
 
-    public static Book createBook (String name, int year){
+    public static Book createBook (String name, int year, String author){
         Book book = new Book();
         book.name = name;
         book.year = year;
+        book.author = author;
         return book;
     }
 
@@ -22,6 +24,10 @@ public class Book {
 
     public int getYear(){
         return this.year;
+    }
+
+    public String getAutor(){
+        return this.author;
     }
 
     public void setYear(int year) {
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 void main(String[] args) {
-Book alphabet = Book.createBook("alphabet", 1948);
-Author authorAlphabet = Author.createAuthor("David", "Diringer");
-Book whiteBimBlackEar = Book.createBook("Белый Бим Чёрное ухо", 1971);
-Author authorwhiteBimBlackEar = Author.createAuthor("Гавриил", "Троепольский");
+    Book alphabet = Book.createBook("alphabet", 1948, "David Diringe");
+    Author authorAlphabet = Author.createAuthor("David", "Diringer");
+    Book whiteBimBlackEar = Book.createBook("Белый Бим Чёрное ухо", 1971, "Гавриил Троепольский");
+    Author authorwhiteBimBlackEar = Author.createAuthor("Гавриил", "Троепольский");
 
     System.out.println("alphabet.name = " + alphabet.getName());
     System.out.println("alphabet.year = " + alphabet.getYear());
